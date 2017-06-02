3218f67459e05e80ec73c4c3bf1148fe55a5704d
Ardi Vaba <ardivaba@gmail.com>
2017-06-02 11:22:58 +0300

    Finished Homework 4

---
+++ Homework4/src/main/java/ee/school/hibernate/entity/Shop.java
@@ -0,0 +1,44 @@
+
+package ee.school.hibernate.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "shop")
+public class Shop {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long id;
+    
+    @Column
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public void setName(String name) {
+    	this.name = name;
+    }
+    
+    public String getName() {
+    	return this.name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+    
+}
